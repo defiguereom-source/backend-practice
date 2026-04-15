a782783aabf01e0424264c840431c6541b7ff3e8
daniel182730 <daniel182730@gmail.com>
2026-04-15 10:31:12 -0400

    feat(python) agregando conocimientos basicos para aprender a programar en python

---
+++ 01-Python-Basico/01-helloWorld.py
@@ -0,0 +1,42 @@
+# Imprimir 
+texto = "hello word";
+print(texto);
+
+#Tipos de Datos (los 4 basicos hay mas)
+texto = "Esto es un string"
+numero_1 = 1;
+decimal = 1.5;
+boolean = True;
+
+print(type(numero_1));
+print(type(texto));
+print(type(decimal));
+print(type(boolean));
+
+# Concatenacion
+texto1 = "Hello";
+texto2 = "World"
+texto = texto1 + texto2;
+print(texto);
+
+#List
+array = [1, 2, 3, 4, 5];
+
+#Tuples (son tipos de datos que no pueden cambiar si ya estan definidos)
+
+#Obj Diccionario
+persona ={
+    "name": "Daniel",
+    "apellido": "Figuero Mendez",
+    "edad": 25   
+}
+
+print(persona);
+print(type(persona))
+
+#tiene case sensitive
+texto = "case";
+Texto = 2;
+
+#Constantes (no exiten pero si ves una variable en mayusculas quiere decir que es una constante)
+PI = 3.14   
