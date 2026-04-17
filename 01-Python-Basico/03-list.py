3f186a2f59e5279fd1a7e8583a9c772bfa99df3a
daniel182730 <daniel182730@gmail.com>
2026-04-17 08:47:19 -0400

    feat(Python-Basico) agregando como usar listas en python

---
+++ 01-Python-Basico/03-list.py
@@ -0,0 +1,10 @@
+# Construtores de listas
+numbers_list = list([1, 2, 3, 4]);
+print(numbers_list);
+
+# Lista basada en rango
+rango = list(range(1, 11));
+print(rango)
+
+# Todos los metodos de una lista
+print(dir(numbers_list));
