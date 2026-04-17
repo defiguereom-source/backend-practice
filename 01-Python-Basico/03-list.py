0f9114d400f285ce10203fa7669d6a62ce223b19
daniel182730 <daniel182730@gmail.com>
2026-04-17 10:22:48 -0400

    feat(Python-Basico) Agregando todo lo basico list/set/dictionaries/conditionals/loops/functions/modules

--- 01-Python-Basico/03-list.py
+++ 01-Python-Basico/03-list.py
@@ -8,3 +8,13 @@ print(rango)
 
 # Todos los metodos de una lista
 print(dir(numbers_list));
+
+#tuplas (todo lo que puedo hacer con una tupla)
+x = ('Hola' , 'Adios', 'Etc', 'Tupl');
+
+print(dir(x));
+
+# Ejemplo de como usar una tupla en un diccionario
+locations = {
+    (35.123, 39.0000): 'Tokyo'
+}
---
+++ 01-Python-Basico/09-functions.py
@@ -0,0 +1,11 @@
+# Esto es una funcion
+def hello(names = "Person"):
+    names = input()
+    print('Hello ' + names);
+    
+hello()
+
+# function lambda
+add = lambda numberOne, numberTwo: numberOne + numberTwo;
+
+print(add(10,30));
