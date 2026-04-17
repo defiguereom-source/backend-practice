748ac016de54990e7da4829283859e6905798719
daniel182730 <daniel182730@gmail.com>
2026-04-17 08:44:44 -0400

    feat(Python-Basico) agregando una calculadora sencilla

---
+++ 01-Python-Basico/02-calculadora.py
@@ -0,0 +1,29 @@
+# Calculadora muy basica
+
+print("2 NUMEROS SE HARAN TODAS LAS OPERACIONES");
+
+numberOne = input("Ingrese el primer numero: ");
+numberTwo = input("Ingrese el segundo numero: ");
+
+new_numberOne = int(numberOne);
+new_numberTwo = int(numberTwo);
+
+# Suma
+print("Esta es la suma de " + numberOne + " + " + numberTwo);
+print(new_numberOne + new_numberTwo);
+
+# Resta
+print("Esta es la resta de " + numberOne + " - " + numberTwo);
+print(new_numberOne - new_numberTwo);
+
+# Multiplicacion 
+print("Esta es la multiplicacion de " + numberOne + " * " + numberTwo);
+print(new_numberOne * new_numberTwo);
+
+# Divicion 
+print("Esta es la divicion de " + numberOne + " / " + numberTwo);
+print(new_numberOne / new_numberTwo);
+
+# Modulo
+print("Esta es el modulo de " + numberOne + " % " + numberTwo);
+print(new_numberOne % new_numberTwo);
